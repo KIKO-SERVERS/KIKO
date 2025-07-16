57e3644d18dcae5e1b7f76fa0fe54ec8cacff392
kolen44 <malavov70@gmail.com>
2025-07-16 06:08:07 +0300

    feat: added html file for test inside docker

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,18 +1,26 @@
-import { Controller, Get } from '@nestjs/common'
-import { AppService } from './app.service'
-imprt axios from 'axios'
+import { Controller, Get } from '@nestjs/common';
+import axios from 'axios';
+import { readFileSync } from 'fs';
+import { join } from 'path';
+import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  async getHello(){
-    const response = await axios.post('http://ai-model:5000/generate', {
-  inputs: 'Your prompt here',
-  parameters: {
-    max_new_tokens: 200
+  @Get('/page')
+  async getPage() {
+    const mapDir = join(__dirname, '../../static/pages/test-page.html');
+    return readFileSync(mapDir, 'utf8');
   }
-});
+
+  @Get()
+  async getHello() {
+    const response = await axios.post('http://ai-model:5000/generate', {
+      inputs: 'Your prompt here',
+      parameters: {
+        max_new_tokens: 200,
+      },
+    });
   }
 }
