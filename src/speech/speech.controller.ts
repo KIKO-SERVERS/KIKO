5fd48ceebcd867b0c13ae34d285f4cda156f0cfd
kolen44 <malavov70@gmail.com>
2025-07-21 11:07:39 +0300

    feat: added speech module

---
+++ src/speech/speech.controller.ts
@@ -0,0 +1,34 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { SpeechService } from './speech.service';
+import { CreateSpeechDto } from './dto/create-speech.dto';
+import { UpdateSpeechDto } from './dto/update-speech.dto';
+
+@Controller('speech')
+export class SpeechController {
+  constructor(private readonly speechService: SpeechService) {}
+
+  @Post()
+  create(@Body() createSpeechDto: CreateSpeechDto) {
+    return this.speechService.create(createSpeechDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.speechService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.speechService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateSpeechDto: UpdateSpeechDto) {
+    return this.speechService.update(+id, updateSpeechDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.speechService.remove(+id);
+  }
+}
